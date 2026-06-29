b127af14d79f9f3a41ca7394458ed7b9dfb5faa9
taeyoungro <114449452+taeyoungro@users.noreply.github.com>
2026-06-29 09:06:58 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,17 +9,19 @@ terraform {
   }
 }
 
+# 변수(var.)를 제거하고 리전을 직접 지정하여 독립성을 확보합니다.
 provider "aws" {
-  region = var.aws_region
+  region = "ap-northeast-2" 
 }
 
+# AWS VPC 리소스 단일 생성
 resource "aws_vpc" "main" {
   cidr_block           = "10.10.10.0/24"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name        = "my-tf-vpc"
-    ManagedBy   = "Terraform"
+    Name      = "my-tf-vpc"
+    ManagedBy = "Terraform"
   }
 }
